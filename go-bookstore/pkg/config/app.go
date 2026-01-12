a32a490332fd753e90b6b260e04d399068c61e0f
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2026-01-12 10:41:22 +0900

    go-bookstore: update mysql config

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:password?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "admin:password@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}

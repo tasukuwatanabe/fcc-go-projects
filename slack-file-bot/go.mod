55274381dd5a8974fdfb49c4927d5a605887f548
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2026-01-19 22:41:29 +0900

    slack-file-bot: build

--- slack-file-bot/go.mod
+++ slack-file-bot/go.mod
@@ -4,5 +4,6 @@ go 1.25.5
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/slack-go/slack v0.17.3 // indirect
 )
--- slack-file-bot/main.go
+++ slack-file-bot/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
 
 func main() {
-	fmt.Println("start project")
+	if err := godotenv.Load(fmt.Sprintf("env/%s.env", os.Getenv("GO_ENV"))); err != nil {
+		fmt.Println("Error loading env file")
+	}
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelID := os.Getenv("CHANNEL_ID")
+
+	filename := "sample.pdf"
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	params := slack.UploadFileV2Parameters{
+		Channel: channelID,
+		File: filename,
+		Filename: filename,
+		FileSize: int(fileInfo.Size()),
+	}
+
+	uploadedFile, err := api.UploadFileV2(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s posted to slack channel\n", uploadedFile.Title)
 }
